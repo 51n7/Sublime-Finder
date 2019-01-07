9d874d4b0b5ea92c9533f22a8fa5ab0b2f04c05a
Jon Best <smile@jonbest.ca>
2019-01-06 19:32:51 -0700

    better icons and navigation

--- finder.py
+++ finder.py
@@ -4,10 +4,6 @@ import os, sys
 import datetime
 from os.path import expanduser
 
-# global active
-x = -1
-y = -1
-
 class FinderCommand( sublime_plugin.TextCommand ):
 
   def run(self, edit):
@@ -18,7 +14,11 @@ class FinderCommand( sublime_plugin.TextCommand ):
     self.view.set_name('Finder')
     self.view.set_scratch(True)
     self.view.settings().set("finder.is_open", True)
-    self.view.settings().set("font_size", 16.0)
+    self.view.settings().set("finder.x", 0)
+    self.view.settings().set("finder.y", 0)
+    self.view.settings().set("finder.selected_path", expanduser("~"))
+    self.view.settings().set("finder.current_path", expanduser("~"))
+    # self.view.settings().set("font_size", 16.0)
     self.view.settings().set("gutter", False)
     # print( self.view.viewport_extent()[0] )
 
@@ -43,13 +43,16 @@ class FinderUpdateCommand(sublime_plugin.TextCommand):
 
   def run(self, edit, source=None):
     
-    self.phantom_set = sublime.PhantomSet(self.view)
-
-    phantoms = []
+    # if sys.platform.startswith('darwin'):
+      # print('mac')
+      # print(os.name)
+      # print(sys.platform)
+    
     image = "res://Packages/Finder/images/icon-folder.png"
     
-    global x
-    global y
+    # path = self.view.settings().get("finder.path")
+    x = self.view.settings().get("finder.x")
+    y = self.view.settings().get("finder.y")
 
     if source == "up": y -= 1
     if source == "down": y += 1
@@ -57,12 +60,27 @@ class FinderUpdateCommand(sublime_plugin.TextCommand):
     if source == "right": x += 1
     
     if self.view.size() < 2**20:
-      path = expanduser("~")
+
+      if source == "nav-down":
+        x = 0
+        y = 0
+        path = expanduser(self.view.settings().get("finder.selected_path"))
+      else:
+        path = expanduser(self.view.settings().get("finder.current_path"))
+
+      if source == "nav-up":
+        x = 0
+        y = 0
+        path = expanduser(os.path.abspath(os.path.join(self.view.settings().get("finder.current_path"), os.pardir)))
+      
       # files = os.listdir(path)
       files = [name for index, name in enumerate(os.listdir(path)) if not name.startswith('.')]
-
+      
       gen_obj = self.chunks(files, 4)
       limit = 15
+
+      icon_folder = ""
+      icon_file = ""
 
       html = """
         <body id="finder">
@@ -70,6 +88,7 @@ class FinderUpdateCommand(sublime_plugin.TextCommand):
             body {
               margin: 0;
               padding: 10px 20px 10px 10px;
+              color: #c8c8c8;
             }
 
             .row.active {  }
@@ -82,7 +101,7 @@ class FinderUpdateCommand(sublime_plugin.TextCommand):
               line-height: 40px;
             }
 
-            .file.active span {
+            .file.active .name {
               text-decoration: underline;
             }
             
@@ -91,7 +110,11 @@ class FinderUpdateCommand(sublime_plugin.TextCommand):
               height: 20px;
             }
 
-            .file span {
+            .file .icon {
+              font-family: "devicons";
+            }
+
+            .file .name {
               display: inline;
               padding-right: 30px;
             }
@@ -100,11 +123,20 @@ class FinderUpdateCommand(sublime_plugin.TextCommand):
       
       for index, row in enumerate(gen_obj):
         is_row = " active" if index == y else ""
-        check_row = True if index == y else False
         tmp = '<div class="row'+is_row+'">'
 
         for index, col in enumerate(row):
-          is_col = " active" if index == x and check_row == True else ""
+          # is_col = " active" if index == x and is_row == " active" else ""
+          # full_path = os.path.join(path, name)
+
+          if index == x and is_row == " active":
+            is_col = " active"
+            self.view.settings().set("finder.selected_path", os.path.join(path, col))
+            # print(os.path.join(path, col))
+            print( os.path.isfile(os.path.join(path, col)) )
+            # print( os.path.isdir(os.path.join(path, col)) )
+          else:
+            is_col = ""
           
           space = limit - len(col)
           
@@ -115,18 +147,23 @@ class FinderUpdateCommand(sublime_plugin.TextCommand):
             col = col[:(limit - 3)]+"..."
             nbsp = ""
 
-          tmp += '<div class="file'+ is_col +'">📁 <span>' + col + '</span>' + nbsp + '</div>'
+          tmp += '<div class="file'+ is_col +'"><span class="icon">' + icon_folder + '</span> <span class="name">' + col + '</span>' + nbsp + '</div>'
+          # tmp += '<div class="file'+ is_col +'">📁 <span class="name">' + col + '</span>' + nbsp + '</div>'
           # tmp += '<div class="file'+ is_col +'"><img src="' + image + '" width="20" height="20"> <span>' + col + '</span>' + nbsp + '</div>'
 
         tmp += '</div>'
         html += tmp
 
+      html += '<div><br /><br />'+path+'</div>'
       html += '</body>'
       
       self.view.erase_phantoms("list")
       self.view.add_phantom("list", self.view.sel()[0], html, sublime.LAYOUT_BLOCK)
     
     # print(html)
+    self.view.settings().set("finder.x", x)
+    self.view.settings().set("finder.y", y)
+    self.view.settings().set("finder.current_path", path)
     
   def chunks(self, l, n):
     splitArray = []
